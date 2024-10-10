5daafa610d321bc6bb1cd80fc6407fa17b1c9714
doop <flavio.oderzo@etu.univ-amu.fr>
2024-10-10 13:59:41 +0200

    feat : version actuelle du projet

---
+++ plugins/plugin-ecran-connecte/config-notifs.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Remplacez ici <ONESIGNAL_APP_ID> par la valeur du champ "ONESIGNAL APP ID" dans OneSignal
+ */
+define('ONESIGNAL_APP_ID',  'b635d404-e3e2-45a6-8708-96d112c3d070');
+
+/**
+ * Remplacez ici <REST_API_KEY> par la valeur du champ "REST API KEY" dans OneSignal
+ */
+define('ONESIGNAL_API_KEY', 'ZjhmMTAxYzctY2VmMy00NTBiLWI3YTEtODUwZWVjNjhmM2Fl');
