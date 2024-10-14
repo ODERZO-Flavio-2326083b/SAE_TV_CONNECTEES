f7c856c0e3809bc4b17c588cd2b6d51f2f2020c2
gallo <loic.gallou@etu.univ-amu.fr>
2024-10-14 11:32:02 +0200

    TelevisionController.php commenté

--- plugins/plugin-ecran-connecte/src/controllers/TelevisionController.php
+++ plugins/plugin-ecran-connecte/src/controllers/TelevisionController.php
@@ -9,7 +9,7 @@ use Views\TelevisionView;
 /**
  * Class TelevisionController
  *
- * Manage televisions (Create, update, delete, display, display schedules)
+ * Gère les télévisions (Création, mise à jour, suppression, affichage, affichage des emplois du temps)
  *
  * @package Controllers
  */
@@ -17,17 +17,22 @@ class TelevisionController extends UserController implements Schedule
 {
 
     /**
+     * Modèle de TelevisionController.
+     *
      * @var User
      */
     private $model;
 
     /**
+     * Vue de TelevisionController.
+     *
      * @var TelevisionView
      */
     private $view;
 
     /**
-     * Constructor of TelevisionController
+     * Constructeur de TelevisionController.
+     * Initialise le modèle et la vue.
      */
     public function __construct() {
         parent::__construct();
@@ -36,43 +41,44 @@ class TelevisionController extends UserController implements Schedule
     }
 
     /**
-     * Insert a television in the database
+     * Insère une télévision dans la base de données.
      *
-     * @return string
+     * @return string Retourne le HTML du formulaire d'insertion de télévision.
      */
     public function insert() {
         $action = filter_input(INPUT_POST, 'createTv');
-
         $codeAde = new CodeAde();
 
         if (isset($action)) {
-
             $login = filter_input(INPUT_POST, 'loginTv');
             $password = filter_input(INPUT_POST, 'pwdTv');
             $passwordConfirm = filter_input(INPUT_POST, 'pwdConfirmTv');
             $codes = $_POST['selectTv'];
 
+            // Validation des données d'entrée
             if (is_string($login) && strlen($login) >= 4 && strlen($login) <= 25 &&
                 is_string($password) && strlen($password) >= 8 && strlen($password) <= 25 &&
                 $password === $passwordConfirm) {
 
-                $codesAde = array();
+                $codesAde = [];
                 foreach ($codes as $code) {
                     if (is_numeric($code) && $code > 0) {
                         if (is_null($codeAde->getByCode($code)->getId())) {
-                            return 'error';
+                            return 'error'; // Code invalide
                         } else {
                             $codesAde[] = $codeAde->getByCode($code);
                         }
                     }
                 }
 
+                // Configuration du modèle de télévision
                 $this->model->setLogin($login);
                 $this->model->setEmail($login . '@' . $login . '.fr');
                 $this->model->setPassword($password);
                 $this->model->setRole('television');
                 $this->model->setCodes($codesAde);
 
+                // Insertion du modèle dans la base de données
                 if (!$this->checkDuplicateUser($this->model) && $this->model->insert()) {
                     $this->view->displayInsertValidate();
                 } else {
@@ -83,6 +89,7 @@ class TelevisionController extends UserController implements Schedule
             }
         }
 
+        // Récupération des années, groupes et demi-groupes
         $years = $codeAde->getAllFromType('year');
         $groups = $codeAde->getAllFromType('group');
         $halfGroups = $codeAde->getAllFromType('halfGroup');
@@ -91,32 +98,32 @@ class TelevisionController extends UserController implements Schedule
     }
 
     /**
-     * Modify a television
+     * Modifie une télévision.
      *
-     * @param $user User
+     * @param User $user L'utilisateur à modifier.
      *
-     * @return string
+     * @return string Retourne le HTML du formulaire de modification.
      */
     public function modify($user) {
         $page = get_page_by_title('Gestion des utilisateurs');
         $linkManageUser = get_permalink($page->ID);
 
         $codeAde = new CodeAde();
-
         $action = filter_input(INPUT_POST, 'modifValidate');
 
         if (isset($action)) {
             $codes = $_POST['selectTv'];
 
-            $codesAde = array();
+            $codesAde = [];
             foreach ($codes as $code) {
                 if (is_null($codeAde->getByCode($code)->getId())) {
-                    return 'error';
+                    return 'error'; // Code invalide
                 } else {
                     $codesAde[] = $codeAde->getByCode($code);
                 }
             }
 
+            // Mise à jour des codes de l'utilisateur
             $user->setCodes($codesAde);
 
             if ($user->update()) {
@@ -124,6 +131,7 @@ class TelevisionController extends UserController implements Schedule
             }
         }
 
+        // Récupération des années, groupes et demi-groupes
         $years = $codeAde->getAllFromType('year');
         $groups = $codeAde->getAllFromType('group');
         $halfGroups = $codeAde->getAllFromType('halfGroup');
@@ -132,9 +140,9 @@ class TelevisionController extends UserController implements Schedule
     }
 
     /**
-     * Display all televisions in a table
+     * Affiche toutes les télévisions dans un tableau.
      *
-     * @return string
+     * @return string Retourne le HTML affichant toutes les télévisions.
      */
     public function displayAllTv() {
         $users = $this->model->getUsersByRole('television');
@@ -142,20 +150,20 @@ class TelevisionController extends UserController implements Schedule
     }
 
     /**
-     * Display a list a schedule
+     * Affiche une liste d'emplois du temps.
      *
-     * @return mixed|string
+     * @return mixed|string Retourne les emplois du temps sous forme de chaîne de caractères.
      */
     public function displayMySchedule() {
         $current_user = wp_get_current_user();
         $user = $this->model->get($current_user->ID);
-        $user = $this->model->getMycodes([$user])[0];
+        $user = $this->model->getMyCodes([$user])[0];
 
         $string = "";
         if (sizeof($user->getCodes()) > 1) {
             if (get_theme_mod('ecran_connecte_schedule_scroll', 'vert') == 'vert') {
                 $string .= '<div class="ticker1">
-						<div class="innerWrap tv-schedule">';
+                        <div class="innerWrap tv-schedule">';
                 foreach ($user->getCodes() as $code) {
                     $path = $this->getFilePath($code->getCode());
                     if (file_exists($path)) {
@@ -185,7 +193,7 @@ class TelevisionController extends UserController implements Schedule
             if (!empty($user->getCodes()[0])) {
                 $string .= $this->displaySchedule($user->getCodes()[0]->getCode());
             } else {
-                $string .= '<p>Vous n\'avez pas cours </p>';
+                $string .= '<p>Vous n\'avez pas cours</p>';
             }
         }
         return $string;
