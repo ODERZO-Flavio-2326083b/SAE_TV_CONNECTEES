4fd4fb7823dff42b4a20709248b64711e995c5a2
gallo <loic.gallou@etu.univ-amu.fr>
2024-10-14 11:18:47 +0200

    StudentController.php commenté mieux

--- plugins/plugin-ecran-connecte/src/controllers/StudentController.php
+++ plugins/plugin-ecran-connecte/src/controllers/StudentController.php
@@ -9,7 +9,7 @@ use Views\StudentView;
 /**
  * Class StudentController
  *
- * Gère les étudiants (Création, mise à jour, suppression, affichage)
+ * Gère les étudiants (Création, mise à jour, suppression, affichage).
  *
  * @package Controllers
  */
@@ -17,12 +17,12 @@ class StudentController extends UserController implements Schedule
 {
 
     /**
-     * @var User
+     * @var User Modèle représentant un utilisateur.
      */
     private $model;
 
     /**
-     * @var StudentView
+     * @var StudentView Vue associée au contrôleur des étudiants.
      */
     public $view;
 
@@ -39,158 +39,246 @@ class StudentController extends UserController implements Schedule
     /**
      * Insère tous les utilisateurs à partir d'un fichier Excel.
      *
-     * @return string
+     * @return string Retourne le HTML pour afficher le formulaire d'importation d'étudiants.
      */
     public function insert() {
         $actionStudent = filter_input(INPUT_POST, 'importEtu');
 
         if ($actionStudent) {
             $allowed_extension = array("Xls", "Xlsx", "Csv");
-            $extension = ucfirst(strtolower(pathinfo($_FILES['fileSecre']['name'], PATHINFO_EXTENSION)));
+            $extension = ucfirst(strtolower(end(explode(".", $_FILES["excelEtu"]["name"]))));
 
-            // Vérifie si l'extension est correcte
+            // Vérifie si l'extension du fichier est valide.
             if (in_array($extension, $allowed_extension)) {
-                $data = $this->getDataExcel($_FILES['fileSecre']['tmp_name']);
-                foreach ($data as $row) {
-                    if (count($row) < 3) continue;
+                $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($extension);
+                $reader->setReadDataOnly(TRUE);
+                $spreadsheet = $reader->load($_FILES["excelEtu"]["tmp_name"]);
 
-                    // Extrait les données
-                    $login = $row[0];
-                    $email = $row[1];
-                    $pwd = substr(md5(uniqid(rand(), true)), 0, 8);
+                $worksheet = $spreadsheet->getActiveSheet();
+                $highestRow = $worksheet->getHighestRow();
 
-                    // Crée un nouvel utilisateur
-                    $this->model->setLogin($login);
-                    $this->model->setEmail($email);
-                    $this->model->setPassword($pwd);
-                    $this->model->setRole('etudiant');
+                $row = $worksheet->getRowIterator(1, 1);
+                $cells = [];
 
-                    // Vérifie les utilisateurs en double et insère l'utilisateur
-                    if (!$this->checkDuplicateUser($this->model) && $this->model->insert()) {
-                        $this->sendWelcomeEmail($login, $email, $pwd);
+                foreach ($row as $value) {
+                    $cellIterator = $value->getCellIterator();
+                    $cellIterator->setIterateOnlyExistingCells(FALSE);
+                    foreach ($cellIterator as $cell) {
+                        $cells[] = $cell->getValue();
                     }
                 }
-                return $this->view->displayInsertValidate();
+
+                // Vérifie si le fichier a le bon format.
+                if ($cells[0] == "Identifiant Ent" && $cells[1] == "Adresse mail") {
+                    $doubles = array();
+                    for ($i = 2; $i < $highestRow + 1; ++$i) {
+                        $cells = array();
+                        foreach ($worksheet->getRowIterator($i, $i + 1) as $row) {
+                            $cellIterator = $row->getCellIterator();
+                            $cellIterator->setIterateOnlyExistingCells(FALSE);
+                            foreach ($cellIterator as $cell) {
+                                $cells[] = $cell->getValue();
+                            }
+                        }
+
+                        $pwd = wp_generate_password();
+                        $login = $cells[0];
+                        $email = $cells[1];
+
+                        if (isset($login) && isset($email)) {
+
+                            $this->model->setLogin($login);
+                            $this->model->setPassword($pwd);
+                            $this->model->setEmail($email);
+                            $this->model->setRole('etudiant');
+
+                            // Vérifie les utilisateurs en double et insère l'utilisateur.
+                            if (!$this->checkDuplicateUser($this->model) && $this->model->insert()) {
+
+                                // Génère un mail de confirmation.
+                                $to = $email;
+                                $subject = "Inscription à la télé-connecté";
+                                $message = '
+                                <!DOCTYPE html>
+                                <html lang="fr">
+                                	<head>
+                                    	<title>Inscription à la télé-connecté</title>
+                                    </head>
+                                    <body>
+                                        <p>Bonjour, vous avez été inscrit sur le site de la Télé Connecté de votre département en tant qu\'étudiant</p>
+                                        <p> Sur ce site, vous aurez accès à votre emploi du temps, à vos notes et aux informations concernant votre scolarité.</p>
+                                        <p> Votre identifiant est ' . $login . ' et votre mot de passe est ' . $pwd . '.</p>
+                                        <p> Veuillez changer votre mot de passe lors de votre première connexion pour plus de sécurité !</p>
+                                        <p> Pour vous connecter, rendez-vous sur le site : <a href="' . home_url() . '"> ' . home_url() . ' </a>.</p>
+                                        <p> Nous vous souhaitons une bonne expérience sur notre site.</p>
+                                    </body>
+                                 </html>';
+
+                                $headers = 'Content-Type: text/html; charset=UTF-8';
+
+                                mail($to, $subject, $message, $headers);
+                            } else {
+                                array_push($doubles, $login);
+                            }
+                        }
+                    }
+                    if (sizeof($doubles) > 0) {
+                        $this->view->displayErrorDouble($doubles);
+                    } else {
+                        $this->view->displayInsertValidate();
+                    }
+                } else {
+                    $this->view->displayWrongFile();
+                }
             } else {
-                return $this->view->displayErrorFileType();
+                $this->view->displayWrongExtension();
             }
         }
-        return $this->view->displayImportForm();
+        return $this->view->displayInsertImportFileStudent();
     }
 
     /**
-     * Affiche un utilisateur spécifique.
+     * Modifie un étudiant.
      *
-     * @return string
+     * @param User $user L'utilisateur à modifier.
+     *
+     * @return string Retourne le HTML du formulaire de modification de l'étudiant.
      */
-    public function displayOneStudent() {
-        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-        $user = $this->model->get($id);
-        if ($user) {
-            return $this->view->displayStudent($user);
+    public function modify($user) {
+        $page = get_page_by_title('Gestion des utilisateurs');
+        $linkManageUser = get_permalink($page->ID);
+
+        $action = filter_input(INPUT_POST, 'modifvalider');
+
+        $codeAde = new CodeAde();
+
+        if ($action == 'Valider') {
+            $year = filter_input(INPUT_POST, 'modifYear');
+            $group = filter_input(INPUT_POST, 'modifGroup');
+            $halfGroup = filter_input(INPUT_POST, 'modifHalfgroup');
+
+            // Vérifie que les valeurs de l'année, du groupe et du demi-groupe sont numériques.
+            if (is_numeric($year) && is_numeric($group) && is_numeric($halfGroup)) {
+
+                $codes = [$year, $group, $halfGroup];
+                $codesAde = array();
+                foreach ($codes as $code) {
+                    if ($code !== 0) {
+                        $code = $codeAde->getByCode($code);
+                    }
+                    $codesAde[] = $code;
+                }
+
+                // Vérifie que les codes sont valides.
+                if ($codesAde[0]->getType() !== 'year') {
+                    $codesAde[0] = 0;
+                }
+
+                if ($codesAde[1]->getType() !== 'group') {
+                    $codesAde[1] = 0;
+                }
+
+                if ($codesAde[2]->getType() !== 'halfGroup') {
+                    $codesAde[2] = 0;
+                }
+
+                $user->setCodes($codesAde);
+                if ($user->update()) {
+                    $this->view->displayModificationValidate($linkManageUser);
+                }
+            }
         }
-        return $this->view->displayErrorUser();
+
+        // Récupère les années, groupes et demi-groupes.
+        $years = $codeAde->getAllFromType('year');
+        $groups = $codeAde->getAllFromType('group');
+        $halfGroups = $codeAde->getAllFromType('halfGroup');
+
+        return $this->view->displayModifyStudent($user, $years, $groups, $halfGroups);
     }
 
     /**
-     * Affiche tous les étudiants.
+     * Affiche l'emploi du temps de l'étudiant.
      *
-     * @return string
-     */
-    public function displayAllStudents() {
-        $students = $this->model->getUsersByRole('etudiant');
-        return $this->view->displayAllStudents($students);
-    }
-
-    /**
-     * Affiche le programme d'un étudiant spécifique.
-     *
-     * @return string
+     * @return bool|mixed|string Retourne l'emploi du temps ou redirige vers la gestion de l'étudiant.
      */
     public function displayMySchedule() {
-        // Implémente l'affichage de l'emploi du temps de l'étudiant
-        return $this->view->displayStudentSchedule($this->model->getUserSchedule());
-    }
+        $current_user = wp_get_current_user();
+        $user = $this->model->get($current_user->ID);
 
-    /**
-     * Gère l'emploi du temps de l'étudiant.
-     *
-     * @return string
-     */
-    public function manageStudent() {
-        $group = filter_input(INPUT_POST, 'groupSecre');
-        $year = filter_input(INPUT_POST, 'yearSecre');
-        if ($group && $year) {
-            $this->model->setGroup($group);
-            $this->model->setYear($year);
-            if ($this->model->update()) {
-                return $this->view->displaySuccessUpdate();
-            } else {
-                return $this->view->displayErrorUpdate();
+        if (sizeof($user->getCodes()) > 0) {
+            $codes = array_reverse($user->getCodes());
+            //$codes = [$user->getCodes()[2], $user->getCodes()[1], $user->getCodes()[0]];
+            foreach ($codes as $code) {
+                if ($code instanceof CodeAde) {
+                    if (file_exists($this->getFilePath($code->getCode()))) {
+                        return $this->displaySchedule($code->getCode());
+                    }
+                }
             }
         }
-        return $this->view->displayManageForm();
+        $this->manageStudent($user);
     }
 
     /**
-     * Modifie un étudiant spécifique.
+     * Vérifie si l'étudiant a un groupe.
+     * Si ce n'est pas le cas, demande de sélectionner des groupes.
      *
-     * @return string
+     * @param User $user L'utilisateur à gérer.
      */
-    public function modify() {
-        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-        if ($this->model->get($id)) {
-            $user = $this->model->get($id);
-            if ($user->update()) {
-                return $this->view->displaySuccessModification();
-            } else {
-                return $this->view->displayErrorModification();
+    public function manageStudent($user) {
+        $codeAde = new CodeAde();
+
+        $years = $codeAde->getAllFromType('year');
+        $groups = $codeAde->getAllFromType('group');
+        $halfGroups = $codeAde->getAllFromType('halfGroup');
+
+        $action = filter_input(INPUT_POST, 'addSchedules');
+
+        if (isset($action)) {
+
+            $year = filter_input(INPUT_POST, 'selectYears');
+            $group = filter_input(INPUT_POST, 'selectGroups');
+            $halfGroup = filter_input(INPUT_POST, 'selectHalfgroups');
+
+            // Vérifie que les valeurs sélectionnées sont valides.
+            if ((is_numeric($year) || $year == 0) && (is_numeric($group) || $group == 0) && (is_numeric($halfGroup) || $halfGroup == 0)) {
+
+                $codes = [$year, $group, $halfGroup];
+                $codesAde = [];
+                foreach ($codes as $code) {
+                    if ($code !== 0) {
+                        $code = $codeAde->getByCode($code);
+                    }
+                    $codesAde[] = $code;
+                }
+
+                // Vérifie que les codes sont valides.
+                if ($codesAde[0]->getType() !== 'year') {
+                    $codesAde[0] = 0;
+                }
+
+                if ($codesAde[1]->getType() !== 'group') {
+                    $codesAde[1] = 0;
+                }
+
+                if ($codesAde[2]->getType() !== 'halfGroup') {
+                    $codesAde[2] = 0;
+                }
+
+                $user->setCodes($codesAde);
+                $user->update();
+                $this->view->refreshPage();
             }
         }
-        return $this->view->displayErrorUser();
+        return $this->view->selectSchedules($years, $groups, $halfGroups);
     }
 
     /**
-     * Supprime un étudiant spécifique.
-     *
-     * @return string
+     * Affiche tous les étudiants dans un tableau.
      */
-    public function delete() {
-        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-        if ($this->model->get($id)) {
-            $this->model->delete($id);
-            return $this->view->displaySuccessDeletion();
-        }
-        return $this->view->displayErrorUser();
+    function displayAllStudents() {
+        $users = $this->model->getUsersByRole('etudiant');
+        return $this->view->displayAllStudent($users);
     }
-
-    /**
-     * Envoie un e-mail de bienvenue à l'étudiant.
-     *
-     * @param string $login
-     * @param string $email
-     * @param string $pwd
-     */
-    private function sendWelcomeEmail($login, $email, $pwd) {
-        $to = $email;
-        $subject = "Inscription à la télé-connecté";
-        $message = '
-        <!DOCTYPE html>
-        <html lang="fr">
-            <head>
-                <title>Inscription à la télé-connecté</title>
-            </head>
-            <body>
-                <p>Bonjour, vous avez été inscrit sur le site de la Télé Connecté de votre département en tant qu\'étudiant</p>
-                <p>Votre identifiant est ' . htmlspecialchars($login) . ' et votre mot de passe est ' . htmlspecialchars($pwd) . '.</p>
-                <p>Veuillez changer votre mot de passe lors de votre première connexion pour plus de sécurité !</p>
-                <p>Pour vous connecter, rendez-vous sur le site : <a href="' . htmlspecialchars(home_url()) . '">' . htmlspecialchars(home_url()) . '</a>.</p>
-                <p>Nous vous souhaitons une bonne expérience sur notre site.</p>
-            </body>
-        </html>';
-
-        $headers = 'Content-Type: text/html; charset=UTF-8';
-        mail($to, $subject, $message, $headers);
-    }
-
 }
