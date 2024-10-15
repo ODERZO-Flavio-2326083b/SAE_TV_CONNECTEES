6a6d620c923bca116a6ad63b317ef06cc32e53b5
Xanooo <tamineha@yahoo.fr>
2024-10-15 11:16:30 +0200

    Ajouts des images, apparition de l'emploi du temps.

--- themes/theme-ecran-connecte/header.php
+++ themes/theme-ecran-connecte/header.php
@@ -34,9 +34,9 @@ if(in_array('television', $current_user->roles)) : ?>
         </a>
     <?php else: ?>
     <nav class="navbar navbar-expand-lg navbar-dark nav_ecran">
-        <a class="navbar-brand" href="https://saetest.alwaysdata.net/accueil/">
+        <a class="navbar-brand" href="<?php echo home_url(); ?>">
             <?php if (get_header_image()) : ?>
-                <img src="<?php header_image(); ?>" class="d-inline-block align-top logo" alt="Logo du site">
+                <img src="<?php header_image(); ?>" class="d-inline-block align-top logo" alt="Logo de l'IUT d'Aix-en-Provence">
             <?php endif; ?>
             <?php bloginfo('name'); ?>
         </a>
@@ -110,57 +110,4 @@ if(in_array('television', $current_user->roles)) : ?>
 </header>
 
 <?php
-/*
-use Controllers\TelevisionController;
-
-$current_user = wp_get_current_user();
-
-echo '
-<!DOCTYPE html>
-<html lang="fr">
-<!-- HEAD -->
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ecran connecté</title>
-    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>'.
-    wp_head();
-    if (in_array("television", $current_user->roles)) {
-	    if(get_theme_mod('ecran_connecte_shecule_scroll', 'vert') == 'vert') {
-		    echo '<script src="/wp-content/plugins/plugin-ecran-connecte/public/js/scroll.js"></script>';
-	    }
-        echo '<script src="/wp-content/themes/theme-ecran-connecte/assets/js/refresh.js"></script>';
-    }
-echo '</head><!-- BODY -->';
-
-if ( in_array( 'television', $current_user->roles ) ) {
-    echo '<body id="tv_body" '; echo body_class(). ' >';
-} else {
-	echo '<body '; echo body_class(). ' >';
-}
-
-wp_body_open();
-
-if (class_exists(TelevisionController::class)) {
-	if (in_array("television", $current_user->roles)) {
-	    echo '
-        <!-- HEADER -->
-        <header class="logo-tv">
-            <a href="'.wp_logout_url(home_url()).'" rel="home">
-                <img src="'; echo header_image().'" alt="Logo du site"/>
-            </a>
-        </header>';
-	} else {
-		echo '
-		<header>
-			<a href="javascript:void(0);" class="icon" onclick="switchMenu()">&#9776;</a>
-			<a href="'.esc_url(home_url()).'" rel="home">
-	            <img class="logo" src="'; echo header_image().'" alt="Logo du site"/>
-	        </a>
-        </header>';
-		include_once 'template-parts/navigation/menu.php';
-	}
-}
-echo '<div class="flex-content">';
-*/
 ?>
