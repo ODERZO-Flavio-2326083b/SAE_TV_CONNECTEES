fda5ec8dac6d0f71309ca53ddbba3f6fc452ffe3
doop <flavio.oderzo@etu.univ-amu.fr>
2024-10-15 17:15:03 +0200

    feat: création onglet Gestion des départements

---
+++ public/wp-content/plugins/plugin-ecran-connecte/src/models/Department.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Models;
+/**
+ * class Department
+ *
+ * Department entity
+ *
+ * @package Models
+ */
+class Department extends Model implements Entity, JsonSerializable {
+
+	private $dep_id, $nom, $longitude, $latitude;
+
+	public function insert() {
+		// TODO: Implement insert() method.
+	}
+
+	public function update() {
+		// TODO: Implement update() method.
+	}
+
+	public function delete() {
+		// TODO: Implement delete() method.
+	}
+
+	public function get( $id ) {
+		// TODO: Implement get() method.
+	}
+
+	public function getList() {
+		// TODO: Implement getList() method.
+	}
+
+	public function setEntity( $data ) {
+		// TODO: Implement setEntity() method.
+	}
+
+	public function setEntityList( $dataList ) {
+		// TODO: Implement setEntityList() method.
+	}
+}
