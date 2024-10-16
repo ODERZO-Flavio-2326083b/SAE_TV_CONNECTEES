8f52f678895452c725e8ca3ec5ad153ffb2c9a89
doop <flavio.oderzo@etu.univ-amu.fr>
2024-10-16 09:18:27 +0200

    feat: début création model/Department

--- public/wp-content/plugins/plugin-ecran-connecte/src/models/Department.php
+++ public/wp-content/plugins/plugin-ecran-connecte/src/models/Department.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace Models;
+
+use JsonSerializable;
+use PDO;
+
 /**
  * class Department
  *
@@ -10,33 +14,191 @@ namespace Models;
  */
 class Department extends Model implements Entity, JsonSerializable {
 
-	private $dep_id, $nom, $longitude, $latitude;
+	/**
+	 * @var int
+	 */
+	private $id_department;
+	/**
+	 * @var string
+	 */
+	private $name;
+	/**
+	 * @var int
+	 */
+	private $longitude;
+	/**
+	 * @var int
+	 */
+	private $latitude;
 
-	public function insert() {
-		// TODO: Implement insert() method.
+	/**
+	 * Insérer un département dans la base de données selon les attributs actuels
+	 *
+	 * @return string
+	 */
+	public function insert(): string {
+		$database = $this->getDatabase();
+
+		$request = $database->prepare(// TODO: INSERT SQL )
+		);
+
+		$request->execute();
+
+		return $database->lastInsertId();
 	}
 
-	public function update() {
-		// TODO: Implement update() method.
+	/**
+	 * Met à jour un département de la base de données selon les attributs actuels
+	 * @return int
+	 */
+	public function update(): int {
+		$database = $this->getDatabase();
+
+		$request = $database->prepare(//TODO : UPDATE SQL)
+		);
+
+		$request->execute();
+
+		return $request->rowCount();
 	}
 
-	public function delete() {
-		// TODO: Implement delete() method.
+	/**
+	 * Supprime un département de la base de données selon les attributs actuels
+	 *
+	 * @return int
+	 */
+	public function delete(): int {
+		$database = $this->getDatabase();
+
+		$request = $database->prepare(// TODO: DELETE SQL
+		);
+
+		$request->execute();
+
+		return $request->rowCount();
 	}
 
-	public function get( $id ) {
-		// TODO: Implement get() method.
+	/**
+	 * Récupère le département en fonction de son id
+	 * @param $id
+	 *
+	 * @return bool|null
+	 */
+	public function get( $id ): ?bool {
+		$database = $this->getDatabase();
+
+		$request = $database->prepare(// TODO GET SQL
+
+		);
+
+		$request->execute();
+
+		if ( $request->rowCount() > 0 ) {
+			return $this->setEntity( $request->fetch( PDO::FETCH_ASSOC ) );
+		}
+
+		return false;
+
 	}
 
-	public function getList() {
-		// TODO: Implement getList() method.
+	public function getList( int $begin = 0, int $numberElement = 25 ): array {
+		$request = $this->getDatabase()->prepare(// TODO GETLIST SQL
+		);
+		$request->bindValue( ':begin', $begin, PDO::PARAM_INT );
+		$request->bindValue( ':numberElement', $numberElement, PDO::PARAM_INT );
+
+		$request->execute();
+
+		if ( $request->rowCount() > 0 ) {
+			return $this->setEntityList( $request->fetchAll() );
+		}
+
+		return [];
 	}
 
-	public function setEntity( $data ) {
-		// TODO: Implement setEntity() method.
+	/**
+	 * @inheritDoc
+	 */
+	public function setEntity( $data ): Department {
+		$entity = new Department();
+
+		$entity->setIdDepartment( $data['id_department'] );
+		$entity->setName( $data['name'] );
+		$entity->setLongitude( $data['longitude'] );
+		$entity->setLatitude( $data['latitude'] );
+
+		return $entity;
 	}
 
-	public function setEntityList( $dataList ) {
-		// TODO: Implement setEntityList() method.
+	/**
+	 * @inheritDoc
+	 */
+	public function setEntityList( $dataList, $adminSite = false ) {
+		$listEntity = array();
+		foreach ( $dataList as $data ) {
+			$listEntity[] = $this->setEntity( $data, $adminSite );
+		}
+
+		return $listEntity;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getIdDepartment(): int {
+		return $this->id_department;
+	}
+
+	/**
+	 * @param int $id_department
+	 */
+	public function setIdDepartment( int $id_department ): void {
+		$this->id_department = $id_department;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName(): string {
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setName( string $name ): void {
+		$this->name = $name;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getLongitude(): int {
+		return $this->longitude;
+	}
+
+	/**
+	 * @param int $longitude
+	 */
+	public function setLongitude( int $longitude ): void {
+		$this->longitude = $longitude;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getLatitude(): int {
+		return $this->latitude;
+	}
+
+	/**
+	 * @param int $latitude
+	 */
+	public function setLatitude( int $latitude ): void {
+		$this->latitude = $latitude;
+	}
+
+	public function jsonSerialize() {
+		return get_object_vars($this);
 	}
 }
