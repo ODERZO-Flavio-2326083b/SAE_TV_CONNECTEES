1e1ef542db23a4ba99f6a216af72e0d2e396d700
Navaha <matheo.bertin@yahoo.com>
2024-10-17 09:28:36 +0200

    Tentative de réglage du bug des menus déroulants : Ajout d'un 'style=position: static' à la troisième div de la page d'accueil (ne fonctionne pas car WordPress ne semble pas autoriser les changements dans le thème)

--- public/wp-content/themes/theme-ecran-connecte/home.php
+++ public/wp-content/themes/theme-ecran-connecte/home.php
@@ -3,7 +3,7 @@
     <main>
         <div class="container">
             <div class="row">
-                <div class="col-6 mx-auto col-md-6 order-md-1">
+                <div class="col-6 mx-auto col-md-6 order-md-1" style="position: static">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/media/background.png" alt="Logo Amu" class="img-fluid mb-3 mb-md-0">
                 </div>
                 <div class="col-md-6 order-md-2 text-center text-md-left pr-md-5">
