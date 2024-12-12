d34dd2a762374ee2dece08456f17e90d6d0097f2
flav <flavio.oderzo@etu.univ-amu.fr>
2024-12-12 18:22:10 +0100

    fix: pages inaccessibles par le commun des mortels, et StudientController.php supprimé

--- public/wp-content/plugins/plugin-ecran-connecte/src/utils/customFunctions.php
+++ public/wp-content/plugins/plugin-ecran-connecte/src/utils/customFunctions.php
@@ -2,6 +2,14 @@
 
 namespace Utils;
 
+/**
+ * Fonction pour remplacer la fonction dépréciée get_page_by_title(), qui renvoie le lien d'une
+ * page wordpress depuis son nom.
+ *
+ * @param $page_title string Titre de la page à chercher
+ *
+ * @return array|\WP_Post|null Lien de la ou les pages trouvées, null si aucune.
+ */
 function get_page_by_title_custom($page_title) {
 	$args = array(
 		'post_type'   => 'page',
@@ -18,4 +26,21 @@ function get_page_by_title_custom($page_title) {
 	}
 
 	return null;
+}
+
+/**
+ * Renvoie un booléen qui correspond à si l'utilisateur actuel possède au moins un des roles
+ *
+ * @param $roles_array
+ *
+ * @return bool
+ */
+function does_user_has_role($roles_array): bool {
+	$current_user = wp_get_current_user();
+	foreach ($roles_array as $role) {
+		if (in_array( $role, $current_user->roles)) {
+			return true;
+		}
+	}
+	return false;
 }
