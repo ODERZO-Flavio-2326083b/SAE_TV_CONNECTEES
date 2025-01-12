844ff0e2ca415e5b9eff5c087f817f5ba2f0e9ae
Navaha <matheo.bertin@etu.univ-amu.fr>
2025-01-12 23:02:57 +0100

    Codesniffer des widgets

--- public/wp-content/plugins/plugin-ecran-connecte/widgets/WidgetAlert.php
+++ public/wp-content/plugins/plugin-ecran-connecte/widgets/WidgetAlert.php
@@ -1,22 +1,26 @@
 <?php
-
+// TODO : Ajouter la doc du fichier
 use controllers\AlertController;
 
 add_action('widgets_init', 'alert_register_widget');
-function alert_register_widget() {
-    register_widget( 'WidgetAlert');
+
+// TODO : Ajouter la doc pour la fonction
+function alert_register_widget()
+{
+    register_widget('WidgetAlert');
 }
 
 /**
+ * TODO : Ajouter les tags @author, @category, @license, @link et @package
  * Class WidgetAlert
  *
  * Widget for Alert
  */
 class WidgetAlert extends WP_Widget
 {
-	/**
-	 * WidgetAlert constructor.
-	 */
+    /**
+     * WidgetAlert constructor.
+     */
     public function __construct()
     {
         parent::__construct(
@@ -25,51 +29,66 @@ class WidgetAlert extends WP_Widget
             // widget name
             __('AlertController widget', ' teleconnecteeamu_widget_domain'),
             // widget description
-            array( 'description' => __( 'Widget qui affiche des alertes', 'teleconnecteeamu_widget_domain' ), )
+            array(
+                'description' => __(
+                    'Widget qui affiche des alertes',
+                    'teleconnecteeamu_widget_domain'
+                ),
+            )
         );
     }
 
-	/**
-	 * Function of the widget
-	 *
-	 * @param array $args
-	 * @param array $instance
-	 */
-    public function widget($args, $instance)
+    /**
+     * Function of the widget
+     *
+     * @param array $args
+     * @param array $instance
+     *
+     * @return void
+     */
+    public function widget($args, $instance) : void
     {
         $_view = new AlertController();
         $_view->alertMain();
     }
 
-	/**
-	 * @param array $instance
-	 *
-	 * @return string|void
-	 */
+    // TODO : Ajouter une description
+    /**
+     * @param array $instance
+     *
+     * @return string|void
+     */
     public function form($instance)
     {
-        if (isset( $instance[ 'title' ])) {
-	        $title = $instance[ 'title' ];
+        if (isset($instance[ 'title' ])) {
+            $title = $instance[ 'title' ];
         } else {
-	        $title = __( 'Default Title', 'teleconnectee_widget_domain' );
+            $title = __('Default Title', 'teleconnectee_widget_domain');
         }
         echo '
         <p>
-            <label for="'.$this->get_field_id( 'title' ).'">'; _e( 'Title:' ); echo '</label>
-            <input class="widefat" id="'.$this->get_field_id( 'title' ).'" name="'.$this->get_field_name( 'title' ).'" type="text" value="'.esc_attr( $title ).'" />
+            <label for="'
+            .$this->get_field_id('title')
+            .'">'; _e('Title:'); echo '</label>
+            <input class="widefat" id="'
+        .$this->get_field_id('title')
+        .'" name="'.$this->get_field_name('title')
+        .'" type="text" value="'.esc_attr($title).'" />
         </p>';
     }
 
-	/**
-	 * @param array $new_instance
-	 * @param array $old_instance
-	 *
-	 * @return array
-	 */
+    /**
+     * @param array $new_instance
+     * @param array $old_instance
+     *
+     * @return array
+     */
     public function update($new_instance, $old_instance)
     {
         $instance = array();
-        $instance['title'] = (! empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        $instance['title'] = (
+            ! empty($new_instance['title'])
+        ) ? strip_tags($new_instance['title']) : '';
         return $instance;
     }
 }
--- public/wp-content/plugins/plugin-ecran-connecte/widgets/WidgetInformation.php
+++ public/wp-content/plugins/plugin-ecran-connecte/widgets/WidgetInformation.php
@@ -1,22 +1,26 @@
 <?php
-
+// TODO : Ajouter la doc du fichier
 use controllers\InformationController;
 
 add_action('widgets_init', 'information_register_widget');
-function information_register_widget() {
-    register_widget( 'WidgetInformation');
+
+// TODO : Ajouter une doc pour la fonction
+function information_register_widget()
+{
+    register_widget('WidgetInformation');
 }
 
 /**
+ * TODO : Ajouter les tags @author, @category, @license, @link et @package
  * Class WidgetInformation
  *
  * Widget for information
  */
 class WidgetInformation extends WP_Widget
 {
-	/**
-	 * WidgetInformation constructor.
-	 */
+    /**
+     * WidgetInformation constructor.
+     */
     public function __construct()
     {
         parent::__construct(
@@ -25,55 +29,68 @@ class WidgetInformation extends WP_Widget
             // widget name
             __('InformationController widget', ' teleconnecteeamu_widget_domain'),
             // widget description
-            array( 'description' => __( 'Widget qui affiche des informations', 'teleconnecteeamu_widget_domain' ), )
+            array(
+                'description' => __(
+                    'Widget qui affiche des informations',
+                    'teleconnecteeamu_widget_domain'
+                ), )
         );
     }
 
-	/**
-	 * Function of the widget
-	 *
-	 * @param array $args
-	 * @param array $instance
-	 *
-	 * @throws \PhpOffice\PhpSpreadsheet\Exception
-	 * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
-	 */
-    public function widget($args, $instance)
+    /**
+     * Function of the widget
+     *
+     * @param array $args
+     * @param array $instance
+     *
+     * @return void
+     *
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
+     */
+    public function widget($args, $instance) : void
     {
         $_view = new InformationController();
         $_view->informationMain();
     }
 
-	/**
-	 * @param array $instance
-	 *
-	 * @return string|void
-	 */
+    /**
+     * @param array $instance
+     *
+     * @return string|void
+     */
     public function form($instance)
     {
         if (isset($instance['title'])) {
-	        $title = $instance['title'];
+            $title = $instance['title'];
         } else {
-	        $title = __('Default Title', 'teleconnectee_widget_domain');
+            $title = __('Default Title', 'teleconnectee_widget_domain');
         }
 
         echo '
         <p>
-            <label for="'.$this->get_field_id('title').'">'; _e('Title:'); echo '</label>
-            <input class="widefat" id="'.$this->get_field_id('title').'" name="'.$this->get_field_name('title').'" type="text" value="'.esc_attr($title).'" />
+            <label for="'
+            .$this->get_field_id('title')
+            .'">'; _e('Title:'); echo '</label>
+            <input class="widefat" id="'
+        .$this->get_field_id('title')
+        .'" name="'.$this->get_field_name('title')
+        .'" type="text" value="'.esc_attr($title).'" />
         </p>';
     }
 
-	/**
-	 * @param array $new_instance
-	 * @param array $old_instance
-	 *
-	 * @return array
-	 */
+    /**
+     * @param array $new_instance
+     * @param array $old_instance
+     *
+     * @return array
+     */
     public function update($new_instance, $old_instance)
     {
         $instance = array();
-        $instance['title'] = (! empty( $new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        $instance['title'] = (
+            ! empty($new_instance['title'])
+        ) ? strip_tags($new_instance['title']) : '';
         return $instance;
     }
 }
