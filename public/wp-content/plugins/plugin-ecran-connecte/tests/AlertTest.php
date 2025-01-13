44526562edbfcbdd2c697484b58b15ace5d7d61d
flav <flavio.oderzo@etu.univ-amu.fr>
2025-01-13 01:23:50 +0100

    feat: tests Alert.php

--- public/wp-content/plugins/plugin-ecran-connecte/tests/AlertTest.php
+++ public/wp-content/plugins/plugin-ecran-connecte/tests/AlertTest.php
@@ -2,194 +2,171 @@
 
 use PHPUnit\Framework\TestCase;
 use models\Alert;
-use models\User;
 use models\CodeAde;
-use Mockery\Mock;
+use Mockery as m;
 
 class AlertTest extends TestCase
 {
     private $pdoMock;
     private $pdoStatementMock;
-
+    private $alert;
 
     protected function setUp(): void
     {
-        $this->pdoMock = Mockery::mock(PDO::class);
-        $this->pdoStatementMock = Mockery::mock(PDOStatement::class);
+        $this->pdoMock = m::mock(PDO::class);
+        $this->pdoStatementMock = m::mock(PDOStatement::class);
+
+        $code1 = new CodeAde();
+        $code1->setId(1);
+        $code1->setCode(1234);
+        $code2 = new CodeAde();
+        $code2->setId(2);
+        $code2->setCode(5678);
+
+        $this->alert = m::mock(Alert::class)->makePartial();
+        $this->alert->shouldAllowMockingProtectedMethods();
+        $this->alert->shouldReceive('getDatabase')
+                    ->andReturn($this->pdoMock);
+        $this->alert->setId(1);
+        $this->alert->setAuthor(1);
+        $this->alert->setContent('Alerte test');
+        $this->alert->setCreationDate('2025-01-01');
+        $this->alert->setExpirationDate('2025-01-10');
+        $this->alert->setCodes(array($code1, $code2));
     }
 
+    /**
+     * Teste le bon fonctionnement de la fonction insert()
+     *
+     * @return void
+     */
     public function testInsert()
     {
-        $alerteMock = Mockery::mock(Alert::class)->makePartial();
-        $alerteMock->shouldAllowMockingProtectedMethods();
-        $alerteMock->shouldReceive('getDatabase')->andReturn($this->pdoMock);
-
         $this->pdoMock->shouldReceive('prepare')
-                      ->once()
                       ->with($this->stringContains('INSERT INTO ecran_alert'))
                       ->andReturn($this->pdoStatementMock);
 
-        $this->pdoStatementMock->shouldReceive('execute')
-                               ->once()
-                               ->andReturn(true);
-
-        $this->pdoMock->shouldReceive('lastInsertId')
-                      ->once()
-                      ->andReturn(1);
-
         $this->pdoStatementMock->shouldReceive('bindValue')
-                               ->with($this->anything(),
-                                   $this->anything(),
-                                   $this->anything())
-                               ->andReturn(true);
+                       ->with(':author', 1, PDO::PARAM_INT);
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                       ->with(':content', 'Alerte test', PDO::PARAM_STR);
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                       ->with(':creation_date', '2025-01-01', PDO::PARAM_STR);
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                       ->with(':expirationDate', '2025-01-10', PDO::PARAM_STR);
+        $this->pdoStatementMock->shouldReceive('execute')
+                       ->andReturn(true);
 
-        $alerteMock->shouldReceive('getAuthor')
-                         ->andReturn(1);
-        $alerteMock->shouldReceive('getContent')
-                         ->andReturn('Test content');
-        $alerteMock->shouldReceive('getCreationDate')
-                         ->andReturn('2025-01-01');
-        $alerteMock->shouldReceive('getExpirationDate')
-                         ->andReturn('2025-01-10');
-
-        $codeMock = Mockery::mock(CodeAde::class);
-        $codeMock->shouldReceive('getCode')->andReturn(123123);
-        $codeMock->shouldReceive('getId')->andReturn(2);
-        $alerteMock->shouldReceive('getCodes')->andReturn([$codeMock]);
+        $this->pdoMock->shouldReceive('lastInsertId')->andReturn(100);
 
         $this->pdoMock->shouldReceive('prepare')
-                      ->once()
-                      ->with($this->stringContains('INSERT INTO ecran_code_alert'))
-                      ->andReturn($this->pdoStatementMock);
+                      ->with($this->stringContains
+            ('INSERT INTO ecran_code_alert (alert_id, code_ade_id)')
+        )->andReturn($this->pdoStatementMock);
 
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':idAlert', 100, PDO::PARAM_INT);
+
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':idCodeAde', 1, PDO::PARAM_INT);
         $this->pdoStatementMock->shouldReceive('execute')
-                               ->once()
                                ->andReturn(true);
 
-        $result = $alerteMock->insert();
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':idCodeAde', 2, PDO::PARAM_INT);
+        $this->pdoStatementMock->shouldReceive('execute')
+                               ->andReturn(true);
 
-        $this->assertEquals(1, $result);
+        $result = $this->alert->insert();
+
+        $this->assertEquals(100, $result);
     }
 
+    /**
+     * Teste le bon fonctionnement de la fonction update().
+     *
+     * @return void
+     */
     public function testUpdate()
     {
-        $alertMock = Mockery::mock(Alert::class)->makePartial();
-        $alertMock->shouldAllowMockingProtectedMethods();
-        $alertMock->shouldReceive('getDatabase')->andReturn($this->pdoMock);
-
         $this->pdoMock->shouldReceive('prepare')
-            ->once()
-            ->with($this->stringContains('UPDATE ecran_alert'))
-            ->andReturn($this->pdoStatementMock);
-
-        $this->pdoStatementMock->shouldReceive('execute')
-            ->once()
-            ->andReturn(true);
+                      ->with($this->stringContains('UPDATE ecran_alert'))
+                      ->andReturn($this->pdoStatementMock);
 
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with($this->anything(), $this->anything(), $this->anything())
-            ->andReturn(true);
-
-        $alertMock->shouldReceive('getId')->andReturn(1);
-        $alertMock->shouldReceive('getContent')->andReturn('Updated content');
-        $alertMock->shouldReceive('getExpirationDate')->andReturn('2025-02-01');
-
-        $codeMock = Mockery::mock(CodeAde::class);
-        $codeMock->shouldReceive('getCode')->andReturn(123123);
-        $codeMock->shouldReceive('getId')->andReturn(2);
-        $alertMock->shouldReceive('getCodes')->andReturn([$codeMock]);
-
-        $this->pdoMock->shouldReceive('prepare')
-            ->once()
-            ->with($this->stringContains('DELETE FROM ecran_code_alert'))
-            ->andReturn($this->pdoStatementMock);
-
+                               ->with(':id', 1, PDO::PARAM_INT);
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':content',
+                                   'Alerte test',
+                                   PDO::PARAM_STR);
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':expirationDate',
+                                   '2025-01-10',
+                                   PDO::PARAM_STR);
         $this->pdoStatementMock->shouldReceive('execute')
-            ->once()
-            ->andReturn(true);
+                               ->andReturn(true);
+        $this->pdoStatementMock->shouldReceive('rowCount'
+        )->andReturn(1);
 
-        $this->pdoMock->shouldReceive('prepare')
-            ->once()
-            ->with($this->stringContains('INSERT INTO ecran_code_alert'))
-            ->andReturn($this->pdoStatementMock);
+        $this->pdoMock->shouldReceive('prepare')->with(
+            'DELETE FROM ecran_code_alert WHERE alert_id = :alertId'
+        )->andReturn($this->pdoStatementMock);
 
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':alertId', 1, PDO::PARAM_INT);
         $this->pdoStatementMock->shouldReceive('execute')
-            ->once()
-            ->andReturn(true);
+                               ->andReturn(true);
 
-        $result = $alertMock->update();
+        $this->pdoMock->shouldReceive('prepare')->with(
+                    $this->stringContains('INSERT INTO ecran_code_alert'))
+                        ->andReturn($this->pdoStatementMock);
 
-        $this->assertEquals(1, $result);
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':alertId', 1, PDO::PARAM_INT);
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':codeAdeId', 1, PDO::PARAM_INT);
+        $this->pdoStatementMock->shouldReceive('execute')
+                               ->andReturn(true);
+        $this->pdoStatementMock->shouldReceive('rowCount')
+                               ->andReturn(1);
+
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':codeAdeId', 2, PDO::PARAM_INT);
+        $this->pdoStatementMock->shouldReceive('execute')
+                               ->andReturn(true);
+        $this->pdoStatementMock->shouldReceive('rowCount')
+                               ->andReturn(1);
+
+        $result = $this->alert->update();
+
+        $this->assertEquals(3, $result);
     }
 
+    /**
+     * Teste le bon fonctionnement de la fonction delete()
+     *
+     * @return void
+     */
     public function testDelete()
     {
-        $alertMock = Mockery::mock(Alert::class)->makePartial();
-        $alertMock->shouldAllowMockingProtectedMethods();
-        $alertMock->shouldReceive('getDatabase')->andReturn($this->pdoMock);
-
-        $this->pdoMock->shouldReceive('prepare')
-            ->once()
-            ->with($this->stringContains('DELETE FROM ecran_alert'))
-            ->andReturn($this->pdoStatementMock);
-
-        $this->pdoStatementMock->shouldReceive('execute')
-            ->once()
-            ->andReturn(true);
-
-        $alertMock->shouldReceive('getId')->andReturn(1);
-
-        $result = $alertMock->delete();
-
-        $this->assertEquals(1, $result);
-    }
-
-    public function testDatabaseInteraction()
-    {
-
-        $this->pdoMock->shouldReceive('prepare')
-            ->once()
-            ->with(Mockery::type('string'))
-            ->andReturn($this->pdoStatementMock);
-
+        $this->pdoMock->shouldReceive('prepare')->with(
+            'DELETE FROM ecran_alert WHERE id = :id'
+        )->andReturn($this->pdoStatementMock);
 
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->once()
-            ->with(Mockery::type('string'), Mockery::type('string'))
-            ->andReturnSelf();
-
+                               ->with(':id', 1, PDO::PARAM_INT);
         $this->pdoStatementMock->shouldReceive('execute')
-            ->once()
-            ->andReturn(true);
-
+                               ->andReturn(true);
         $this->pdoStatementMock->shouldReceive('rowCount')
-            ->once()
-            ->andReturn(1);
+                               ->andReturn(1);
 
-        $this->pdoStatementMock->shouldReceive('fetch')
-            ->once()
-            ->andReturn([
-                'ID' => 123,
-                'user_login' => 'testuser',
-                'user_email' => 'test@example.com',
-                'dept_id' => 1
-            ]);
+        $result = $this->alert->delete();
 
-
-        $result = $this->pdoMock->prepare("SELECT * FROM wp_users WHERE ID = :id");
-
-        $this->assertTrue($result->execute());
-        $this->assertEquals(1, $result->rowCount());
-        $this->assertEquals([
-            'ID' => 123,
-            'user_login' => 'testuser',
-            'user_email' => 'test@example.com',
-            'dept_id' => 1
-        ], $result->fetch());
+        $this->assertEquals(1, $result);
     }
 
     protected function tearDown(): void
     {
-        Mockery::close();
+        m::close();
     }
 }
