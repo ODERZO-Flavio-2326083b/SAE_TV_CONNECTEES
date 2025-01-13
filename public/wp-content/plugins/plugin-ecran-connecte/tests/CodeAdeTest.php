54e88c88e09c94142fa37d0f44c3e6b4e5d86086
flav <flavio.oderzo@etu.univ-amu.fr>
2025-01-13 02:48:22 +0100

    feat: tests CodeAdeTest.php

--- public/wp-content/plugins/plugin-ecran-connecte/tests/CodeAdeTest.php
+++ public/wp-content/plugins/plugin-ecran-connecte/tests/CodeAdeTest.php
@@ -2,137 +2,116 @@
 
 use PHPUnit\Framework\TestCase;
 use models\CodeAde;
-use Mockery\Mock;
+use Mockery as m;
 
 class CodeAdeTest extends TestCase
 {
     private $pdoMock;
     private $pdoStatementMock;
+    private $codeAde;
 
     protected function setUp(): void
     {
-        $this->pdoMock = Mockery::mock(PDO::class);
+        $this->pdoMock = m::mock(PDO::class);
         $this->pdoStatementMock = Mockery::mock(PDOStatement::class);
+
+        $this->codeAde = m::mock(CodeAde::class)->makePartial();
+        $this->codeAde->shouldAllowMockingProtectedMethods();
+        $this->codeAde->shouldReceive('getDatabase')
+                      ->andReturn($this->pdoMock);
+
+        $this->codeAde->setId(1);
+        $this->codeAde->setTitle('Code ade');
+        $this->codeAde->setCode(1234);
+        $this->codeAde->setType('year');
+        $this->codeAde->setDeptId(10);
+
+
     }
 
+    /**
+     * Teste le bon fonctionnement de la fonction insert()
+     *
+     * @return void
+     */
     public function testInsert()
     {
-        $codeAdeMock = Mockery::mock(CodeAde::class)->makePartial();
-        $codeAdeMock->shouldAllowMockingProtectedMethods();
-        $codeAdeMock->shouldReceive('getDatabase')->andReturn($this->pdoMock);
-
         $this->pdoMock->shouldReceive('prepare')
-            ->once()
-            ->with($this->stringContains('INSERT INTO ecran_code_ade'))
-            ->andReturn($this->pdoStatementMock);
+                  ->with($this->stringContains('INSERT INTO ecran_code_ade'))
+                  ->andReturn($this->pdoStatementMock);
+
+        $this->pdoStatementMock->shouldReceive('bindValue')
+            ->with(':title', 'Code ade', $this->anything());
+        $this->pdoStatementMock->shouldReceive('bindValue')
+            ->with(':code', 1234, $this->anything());
+        $this->pdoStatementMock->shouldReceive('bindValue')
+            ->with(':type', 'year', $this->anything());
+        $this->pdoStatementMock->shouldReceive('bindValue')
+            ->with(':dept_id', 10, PDO::PARAM_INT);
 
         $this->pdoStatementMock->shouldReceive('execute')
-            ->once()
             ->andReturn(true);
 
         $this->pdoMock->shouldReceive('lastInsertId')
-            ->once()
             ->andReturn(1);
 
-        $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with($this->anything(), $this->anything(), $this->anything())
-            ->andReturn(true);
-
-        $codeAdeMock->shouldReceive('getTitle')->andReturn('Sample Title');
-        $codeAdeMock->shouldReceive('getCode')->andReturn(12345);
-        $codeAdeMock->shouldReceive('getType')->andReturn('group');
-        $codeAdeMock->shouldReceive('getDeptId')->andReturn(1);
-
-        $result = $codeAdeMock->insert();
+        $result = $this->codeAde->insert();
 
         $this->assertEquals(1, $result);
     }
 
     public function testUpdate()
     {
-        $codeAdeMock = Mockery::mock(CodeAde::class)->makePartial();
-        $codeAdeMock->shouldAllowMockingProtectedMethods();
-        $codeAdeMock->shouldReceive('getDatabase')->andReturn($this->pdoMock);
-
         $this->pdoMock->shouldReceive('prepare')
-            ->once()
-            ->with($this->stringContains('UPDATE ecran_code_ade'))
-            ->andReturn($this->pdoStatementMock);
-
-        $this->pdoStatementMock->shouldReceive('execute')
-            ->once()
-            ->andReturn(true);
+                      ->with($this->stringContains('UPDATE ecran_code_ade'))
+                      ->andReturn($this->pdoStatementMock);
 
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with($this->anything(), $this->anything(), $this->anything())
+            ->with(':id', '1', PDO::PARAM_INT);
+        $this->pdoStatementMock->shouldReceive('bindValue')
+            ->with(':title', 'Code ade', $this->anything());
+        $this->pdoStatementMock->shouldReceive('bindValue')
+            ->with(':code', 1234, $this->anything());
+        $this->pdoStatementMock->shouldReceive('bindValue')
+            ->with(':type', 'year', $this->anything());
+        $this->pdoStatementMock->shouldReceive('bindValue')
+            ->with(':dept_id', 10, PDO::PARAM_INT);
+
+        $this->pdoStatementMock->shouldReceive('execute')
             ->andReturn(true);
 
-        $codeAdeMock->shouldReceive('getId')->andReturn(1);
-        $codeAdeMock->shouldReceive('getTitle')->andReturn('Updated Title');
-        $codeAdeMock->shouldReceive('getCode')->andReturn(67890);
-        $codeAdeMock->shouldReceive('getType')->andReturn('year');
-        $codeAdeMock->shouldReceive('getDeptId')->andReturn(2);
+        $this->pdoStatementMock->shouldReceive('rowCount')
+            ->andReturn(1);
 
-        $result = $codeAdeMock->update();
+        $result = $this->codeAde->update();
 
         $this->assertEquals(1, $result);
     }
 
     public function testDelete()
     {
-        $codeAdeMock = Mockery::mock(CodeAde::class)->makePartial();
-        $codeAdeMock->shouldAllowMockingProtectedMethods();
-        $codeAdeMock->shouldReceive('getDatabase')->andReturn($this->pdoMock);
 
-        $this->pdoMock->shouldReceive('prepare')
-            ->once()
-            ->with($this->stringContains('DELETE FROM ecran_code_ade'))
-            ->andReturn($this->pdoStatementMock);
+        $this->pdoMock->shouldReceive('prepare')->with(
+            $this->stringContains('DELETE FROM ecran_code_ade WHERE id = :id'))
+                      ->andReturn($this->pdoStatementMock);
 
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':id', 1, PDO::PARAM_INT);
         $this->pdoStatementMock->shouldReceive('execute')
-            ->once()
-            ->andReturn(true);
+                               ->once()
+                               ->andReturn(true);
+        $this->pdoStatementMock->shouldReceive('rowCount')
+                               ->once()
+                               ->andReturn(1);
 
-        $codeAdeMock->shouldReceive('getId')->andReturn(1);
 
-        $result = $codeAdeMock->delete();
+        $result = $this->codeAde->delete();
 
         $this->assertEquals(1, $result);
     }
 
-    public function testGetByCode()
-    {
-        $codeAdeMock = Mockery::mock(CodeAde::class)->makePartial();
-        $codeAdeMock->shouldAllowMockingProtectedMethods();
-        $codeAdeMock->shouldReceive('getDatabase')->andReturn($this->pdoMock);
-
-        $this->pdoMock->shouldReceive('prepare')
-            ->once()
-            ->with($this->stringContains('SELECT id, title, code, type, dept_id FROM ecran_code_ade WHERE code = :code'))
-            ->andReturn($this->pdoStatementMock);
-
-        $this->pdoStatementMock->shouldReceive('execute')
-            ->once()
-            ->andReturn(true);
-
-        $this->pdoStatementMock->shouldReceive('fetch')
-            ->once()
-            ->andReturn([
-                'id' => 1,
-                'title' => 'Sample Title',
-                'code' => 12345,
-                'type' => 'group',
-                'dept_id' => 1
-            ]);
-
-        $result = $codeAdeMock->getByCode(12345);
-
-        $this->assertInstanceOf(CodeAde::class, $result);
-        $this->assertEquals('Sample Title', $result->getTitle());
-    }
-
     protected function tearDown(): void
     {
-        Mockery::close();
+        m::close();
     }
 }
