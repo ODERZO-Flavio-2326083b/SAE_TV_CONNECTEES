3a0a78d439bb64d49dce22f714521cd0bea1be39
flav <flavio.oderzo@etu.univ-amu.fr>
2025-03-04 00:09:17 +0100

    fix: tests InformationTest.php modifiés pour correspondre au fonctionnement idéal des fonctions

--- public/wp-content/plugins/plugin-ecran-connecte/tests/InformationTest.php
+++ public/wp-content/plugins/plugin-ecran-connecte/tests/InformationTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use models\CodeAde;
 use PHPUnit\Framework\TestCase;
 use models\Information;
 use Mockery as m;
@@ -24,6 +25,14 @@ class InformationTest extends TestCase
         $user = new User();
         $user->setId(10);
 
+        $codeAde1 = new CodeAde();
+
+        $codeAde1->setId(1);
+        $codeAde1->setTitle('Code ade');
+        $codeAde1->setCode(1234);
+        $codeAde1->setType('year');
+        $codeAde1->setDeptId(10);
+
         $this->information->setId(1);
         $this->information->setTitle("Titre de test");
         $this->information->setAuthor($user);
@@ -34,53 +43,65 @@ class InformationTest extends TestCase
         $this->information->setType("texte");
         $this->information->setAdminId(42);
         $this->information->setDuration(3600000);
+        $this->information->setCodesAde([$codeAde1]);
 
     }
 
     public function testInsert()
     {
         $this->pdoMock->shouldReceive('prepare')
-            ->with($this->stringContains('INSERT INTO ecran_information'))
-            ->once()
-            ->andReturn($this->pdoStatementMock);
+                      ->with($this->stringContains('INSERT INTO ecran_information'))
+                      ->once()
+                      ->andReturn($this->pdoStatementMock);
 
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with(':title', 'Titre de test');
-
+                               ->with(':title', 'Titre de test');
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with(':content', 'Test content');
-
+                               ->with(':content', 'Test content');
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with(':creationDate', '2025-01-13');
-
+                               ->with(':creationDate', '2025-01-13');
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with(':expirationDate', '2025-12-31');
-
+                               ->with(':expirationDate', '2025-12-31');
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with(':type', 'texte');
-
+                               ->with(':type', 'texte');
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with(':userId', 10, PDO::PARAM_INT);
-
+                               ->with(':userId', 10, PDO::PARAM_INT);
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with(':department_id', 5, PDO::PARAM_INT);
-
+                               ->with(':administration_id', 42, PDO::PARAM_INT);
         $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with(':duration', 3600000, PDO::PARAM_INT);
-
-        $this->pdoStatementMock->shouldReceive('bindValue')
-            ->with(':administration_id', 42, PDO::PARAM_INT);
-
+                               ->with(':duration', 3600000, PDO::PARAM_INT);
         $this->pdoStatementMock->shouldReceive('execute')
-            ->andReturn(true);
+                               ->once()
+                               ->andReturn(true);
 
         $this->pdoMock->shouldReceive('lastInsertId')
-            ->andReturn(1);
+                      ->andReturn(1);
+
+        // Mock pour INSERT INTO ecran_info_code_ade
+        $this->pdoMock->shouldReceive('prepare')
+                      ->with($this->stringContains('INSERT INTO ecran_info_code_ade'))
+                      ->once()
+                      ->andReturn($this->pdoStatementMock);
+
+        $this->pdoStatementMock->shouldReceive('bindParam')
+                               ->withArgs(function($param, &$value, $type) {
+                                   return $param === ':infoId' && $type === PDO::PARAM_INT;
+                               })
+                               ->once();
+
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':code_ade_id', 1, PDO::PARAM_INT)
+                               ->once();
+
+        $this->pdoStatementMock->shouldReceive('execute')
+                               ->once()
+                               ->andReturn(true);
 
         $result = $this->information->insert();
 
         $this->assertEquals(1, $result);
     }
+
 
     public function testUpdate()
     {
@@ -112,6 +133,25 @@ class InformationTest extends TestCase
 
         $this->pdoStatementMock->shouldReceive('rowCount')
             ->andReturn(1);
+
+        $this->pdoMock->shouldReceive('prepare')
+                      ->with($this->stringContains('DELETE FROM ecran_info_code_ade'))
+                      ->once()
+                      ->andReturn($this->pdoStatementMock);
+
+        $this->pdoStatementMock->shouldReceive('bindValue')
+            ->with(':infoId', 1, PDO::PARAM_INT);
+
+        $this->pdoMock->shouldReceive('prepare')
+                ->with($this->stringContains('INSERT INTO ecran_info_code_ade'))
+                ->once()
+                ->andReturn($this->pdoStatementMock);
+
+        $this->pdoStatementMock->shouldReceive('bindParam')
+                               ->with(':infoId', 1, PDO::PARAM_INT);
+
+        $this->pdoStatementMock->shouldReceive('bindValue')
+                               ->with(':code_ade_id', 1, PDO::PARAM_INT);
 
         $result = $this->information->update();
 
